edc875974dc82201cbe797bdb9a6ff060e547c8f
ali reza azdast <azdast47@gmail.com>
2025-05-12 20:02:27 +0330

    finished a header of the project

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,13 @@
 
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import './index.css'
 import App from './App.jsx'
+
+import './styles/reset.css'
+import './styles/fonts.css'
+import './styles/variables.css'
+import './styles/helpers.css'
+import './styles/defaults.css'
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
--- src/pages/Index/Index.jsx
+++ src/pages/Index/Index.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import './Index.css'
+import Header from '../../componets/Header/Header'
 
 export default function Index() {
   return (
-    <div>Index</div>
+    <>
+      <Header/>
+    </>
   )
 }
