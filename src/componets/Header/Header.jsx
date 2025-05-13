f300895ac7ff7f76bc24dcefbeb29b2d3ea53ce3
ali reza azdast <azdast47@gmail.com>
2025-05-13 15:30:03 +0330

    made a hero section and user a typewiter-effect library

--- src/componets/Header/Header.jsx
+++ src/componets/Header/Header.jsx
@@ -5,7 +5,7 @@ import Navbar from "../Navbar/Navbar";
 
 export default function Header() {
   return (
-    <header class="header">
+    <header className="header">
       <Topbar />
       <Navbar />
     </header>
--- src/pages/Index/Index.jsx
+++ src/pages/Index/Index.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
-import './Index.css'
-import Header from '../../componets/Header/Header'
+import React from "react";
+import "./Index.css";
+import Header from "../../componets/Header/Header";
+import Laning from "../../componets/Landing/Laning";
 
 export default function Index() {
   return (
     <>
-      <Header/>
+      <Header />
+      <Laning />
     </>
-  )
+  );
 }
