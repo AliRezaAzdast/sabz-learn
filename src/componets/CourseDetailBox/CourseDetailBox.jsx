ce6eb41851409c6b3f592c8f73083984886246f1
ali reza azdast <azdast47@gmail.com>
2025-05-16 14:50:20 +0330

    finished most part of the course-info

---
+++ src/componets/CourseDetailBox/CourseDetailBox.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import './CourseDetailBox.css'
+
+export default function CourseDetailBox({title, desc, svg}) {
+  return (
+    <div className="col-4">
+      <div className="course-boxes__box">
+        <div className="course-boxes__box-right">
+          <i className={`course-boxes__box-right-icon fas ${svg}`}></i>
+        </div>
+        <div className="course-boxes__box-left">
+          <span className="course-boxes__box-left-title">{title}</span>
+          <span className="course-boxes__box-left--subtitle">{desc}</span>
+        </div>
+      </div>
+    </div>
+  );
+}
